d01867f69c5e790adbfe26db95b8f22423371725
Airubby <ai_rubby@qq.com>
2018-04-28 06:03:43 +0800

    新增板块

--- cmdb/views.py
+++ cmdb/views.py
@@ -4,6 +4,40 @@ from django.shortcuts import redirect
 from django.shortcuts import HttpResponse
 
 
+# def login(request):
+#     # f = open('templates/login.html', 'r', encoding='utf-8')
+#     # data = f.read();
+#     # f.close()
+#     # return HttpResponse(data)
+#     error_msg=""
+#     if request.method=="POST":
+#         #获取用户通过POST提交过来的信息
+#         #username=request.POST['username']
+#         username=request.POST.get('username',None)
+#         password = request.POST.get('password', None)
+#         print(username,password)
+#         if username=="admin" and password=="xie123":
+#             return redirect('/home')
+#         else:
+#             error_msg="用户名或密码错误"
+#     return render(request,'login.html',{'error_msg':error_msg})
+
+USER_LIST=[
+    {'id':111,'username':'airubby','email':'airubby@qq.com','gender':'man'},
+]
+for index in range(2):
+    temp={'id':index,'username':'airubby'+str(index),'email':'airubby@qq.com','gender':'man'}
+    USER_LIST.append(temp)
+
+def home(request):
+    username = request.POST.get('username', None)
+    email = request.POST.get('email', None)
+    gender = request.POST.get('gender', None)
+    temp={'id':username,'username':username,'email':email,'gender':gender}
+    USER_LIST.append(temp)
+    return render(request, 'home.html',{"user_list":USER_LIST})
+
+
 def login(request):
     # f = open('templates/login.html', 'r', encoding='utf-8')
     # data = f.read();
@@ -15,24 +49,19 @@ def login(request):
         #username=request.POST['username']
         username=request.POST.get('username',None)
         password = request.POST.get('password', None)
-        print(username,password)
+        gender=request.POST.get('gender',None)
+        favor = request.POST.getlist('favor', None)
+        #file = request.POST.get('fileinfo', None)
+        file=request.FILES.get('fileinfo')
+        import os
+        file_path=os.path.join('upload',file.name)
+        print(file_path)
+        f=open(file_path,mode="wb")
+        for i in file.chunks():
+            f.write(i)
+        f.close()
         if username=="admin" and password=="xie123":
             return redirect('/home')
         else:
             error_msg="用户名或密码错误"
     return render(request,'login.html',{'error_msg':error_msg})
-
-USER_LIST=[
-    {'username':'airubby','email':'airubby@qq.com','gender':'man'},
-]
-for index in range(2):
-    temp={'username':'airubby'+str(index),'email':'airubby@qq.com','gender':'man'}
-    USER_LIST.append(temp)
-
-def home(request):
-    username = request.POST.get('username', None)
-    email = request.POST.get('email', None)
-    gender = request.POST.get('gender', None)
-    temp={'username':username,'email':email,'gender':gender}
-    USER_LIST.append(temp)
-    return render(request, 'home.html',{"user_list":USER_LIST})

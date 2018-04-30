5b46d3978d8ed7683dcb034bda870c46ef48d350
Airubby <ai_rubby@qq.com>
2018-04-30 13:48:58 +0800

    从数据库中获取信息展示列表

--- cmdb/urls.py
+++ cmdb/urls.py
@@ -4,15 +4,10 @@ from django.urls import re_path
 from cmdb import views
 
 urlpatterns = [
-    re_path('admin/', admin.site.urls),
-    # path('home', views.home),
     re_path('login',views.login),
-    re_path('home', views.Home.as_view()), #as_view()固定写法
-    re_path('index',views.index,name='indexform'),
-    #path('detail/',views.detail), #以?传参显示
-    #re_path('detail-(\d+)',views.detail),  #不以?传参显示 re_path('detail-(\d+).html',views.detail), 然后url跳转时也加.html
-    re_path('detail-(?P<nid>\d+)',views.detail), #这样传参views.py中就匹配nid的参数，无论形式参数先后顺序，传多个参数
-    re_path('orm',views.orm),
+    re_path('index',views.index),
+    re_path('user_info', views.user_info),
+    re_path('userdetail-(?P<nid>\d+)', views.user_detail),
 ]
 
 
--- cmdb/views.py
+++ cmdb/views.py
@@ -1,161 +1,34 @@
-from django.shortcuts import render
-from django.shortcuts import redirect
+from django.shortcuts import render,HttpResponse,redirect
 # Create your views here.
-from django.shortcuts import HttpResponse
+from cmdb import models
 
 
-# def login(request):
-#     # f = open('templates/login.html', 'r', encoding='utf-8')
-#     # data = f.read();
-#     # f.close()
-#     # return HttpResponse(data)
-#     error_msg=""
-#     if request.method=="POST":
-#         #获取用户通过POST提交过来的信息
-#         #username=request.POST['username']
-#         username=request.POST.get('username',None)
-#         password = request.POST.get('password', None)
-#         print(username,password)
-#         if username=="admin" and password=="xie123":
-#             return redirect('/home')
-#         else:
-#             error_msg="用户名或密码错误"
-#     return render(request,'login.html',{'error_msg':error_msg})
-
-# USER_LIST=[
-#     {'id':111,'username':'airubby','email':'airubby@qq.com','gender':'man'},
-# ]
-# for index in range(2):
-#     temp={'id':index,'username':'airubby'+str(index),'email':'airubby@qq.com','gender':'man'}
-#     USER_LIST.append(temp)
-#
-# def home(request):
-#     username = request.POST.get('username', None)
-#     email = request.POST.get('email', None)
-#     gender = request.POST.get('gender', None)
-#     temp={'id':username,'username':username,'email':email,'gender':gender}
-#     USER_LIST.append(temp)
-#     return render(request, 'home.html',{"user_list":USER_LIST})
-#
 
 def login(request):
-    # f = open('templates/login.html', 'r', encoding='utf-8')
-    # data = f.read();
-    # f.close()
-    # return HttpResponse(data)
-    error_msg=""
-    if request.method=="POST":
+    if request.method=="GET":
+        return render(request,'login.html')
+    elif request.method=="POST":
         #获取用户通过POST提交过来的信息
         #username=request.POST['username']
-        username=request.POST.get('username',None)
-        password = request.POST.get('password', None)
-        gender=request.POST.get('gender',None)
-        favor = request.POST.getlist('favor', None)
-        #file = request.POST.get('fileinfo', None)
-        file=request.FILES.get('fileinfo')
-        import os
-        file_path=os.path.join('upload',file.name)
-        print(file_path)
-        f=open(file_path,mode="wb")
-        for i in file.chunks():
-            f.write(i)
-        f.close()
-        if username=="admin" and password=="xie123":
-            return redirect('/home')
+        user=request.POST.get('username',None)
+        passw = request.POST.get('password', None)
+
+        obj=models.UserInfo.objects.filter(username=user,password=passw).first()
+        if obj:
+            return redirect('/cmdb/index')
         else:
-            error_msg="用户名或密码错误"
+            return render(request,'login.html')
     return render(request,'login.html',{'error_msg':error_msg})
 
 
-from django.views import View
-
-class Home(View):
-
-    def dispatch(self, request, *args, **kwargs): #dispatch先执行,无论函数在前还是在后
-        # 调用父类的dispatch,before，after都会执行，可以定制功能
-        print('before')
-        result=super(Home,self).dispatch(request,*args,**kwargs)
-        print('after')
-        return result
-
-    def get(self,request):
-        print(request.method)
-        return render(request,'home.html')
-
-    def post(self,request):
-        print(request.method,'POST')
-        return render(request, 'home.html')
-
-
-# USER_DICT={
-#     'k1':'123',
-#     'k2':'333',
-# }
-
-USER_DICT={
-    '1':{'name':'root1','email':'root@qq.com'},
-    '2':{'name':'root2','email':'root@qq.com'},
-    '3':{'name':'root3','email':'root@qq.com'},
-    '4':{'name':'root4','email':'root@qq.com'},
-    '5':{'name':'root5','email':'root@qq.com'},
-}
 def index(request):
-    return render(request,'index.html',{'user_dict':USER_DICT})
+    return render(request,'index.html')
 
-#以?传参
-# def detail(request):
-#     nid=request.GET.get('nid')
-#     detail_info=USER_DICT[nid]
-#     return render(request,'detail.html',{'detail_info':detail_info})
+def user_info(request):
+    user_list=models.UserInfo.objects.all()
+    return render(request, 'user_info.html',{'user_list':user_list})
 
-#不以?传参
-def detail(request,nid):  #nid就是urls.py中的(\d+)，urls中给几个参数，这里需要几个接受参数
-    detail_info=USER_DICT[nid]
-    return render(request,'detail.html',{'detail_info':detail_info})
-
-# def detail(request,*args):  #(,**kwargs )
-#     detail_info=USER_DICT[nid]
-#     return render(request,'detail.html',{'detail_info':detail_info})
-#
-
-
-#测试往数据库中添加数据
-from cmdb import models
-def orm(request):
-    #创建方法一，推荐这种
-    # models.UserInfo.objects.create(
-    #     username='root',
-    #     password='xie123',
-    #     email='airubby@qq.com'
-    # )
-    #或者
-    # dic={'username':'root','password':'xie123','email':'airubby@qq.com'}
-    # models.UserInfo.objects.create(**dic)
-    #创建方法二
-    # obj=models.UserInfo(
-    #     username='root',
-    #     password='xie123',
-    #     email='airubby@qq.com'
-    # )
-    # obj.save()
-
-    #查
-    # result=models.UserInfo.objects.all()
-    # for row in result:
-    #     print(row.id,row.username,row.password,row.email)
-    # result = models.UserInfo.objects.filter(username='root')
-    # for row in result:
-    #     print(row.id,row.username,row.password,row.email)
-
-    # #删
-    # models.UserInfo.objects.filter(username='root1').delete()
-
-    #改
-    # models.UserInfo.objects.all().update(password=666)
-    models.UserInfo.objects.filter(id=3).update(password=6666666)
-    
-
-
-    return HttpResponse('ORM')
-
-
+def user_detail(request,nid):
+    obj=models.UserInfo.objects.filter(id=nid).first()
+    #models.UserInfo.objects.get(id=nid) #区单挑数据如果不存在则报错
+    return render(request, 'user_detail.html', {'obj': obj})

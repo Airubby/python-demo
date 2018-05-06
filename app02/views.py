7b3876a1a56c8a5bba319386d20baf01352c171b
Airubby <ai_rubby@qq.com>
2018-05-07 07:15:23 +0800

    装饰器

--- app02/views.py
+++ app02/views.py
@@ -68,15 +68,82 @@ def login(request):
         if dic['pwd']==p:
             res=redirect('app02/home')  #这个是app02模块的文件名
             res.set_cookie('usernamecook',u,max_age=10) #10秒后清空cookies
+
+            #加密cook
+            #obj.set_signed_cookie('usernamecook',"kangbazi",salt="xie")
+            #request.get_signed_cookie('usernamecook',salt='xie')
+
+
             return res
 
         else:
             return render(request,'app02/login.html')
     #return HttpResponse('OK')
 
+#
+# def home(request):
+#     v=request.COOKIES.get('usernamecook')
+#     if not v:
+#         return redirect('app02/login')
+#     return render(request,'app02/home.html',{'current_user':v})
 
-def home(request):
-    v=request.COOKIES.get('usernamecook')
-    if not v:
-        return redirect('app02/login')
-    return render(request,'app02/home.html',{'current_user':v})
+
+
+#装饰器
+def auth(func):
+    def inner(reqeust,*args,**kwargs):
+        v = reqeust.COOKIES.get('usernamecook')
+        if not v:
+            return redirect('app02/login')
+        return func(reqeust, *args,**kwargs)
+    return inner
+
+@auth
+def home(reqeust):
+    # 获取当前已经登录的用户
+    v = reqeust.COOKIES.get('usernamecook')
+    return render(reqeust,'app02/home.html',{'current_user': v})
+
+from django import views
+from django.utils.decorators import method_decorator
+
+@method_decorator(auth,name='dispatch')  #写这外面，下面的def dispatch()就不用了，就自动从父类继承过来了
+class Order(views.View):
+
+    # @method_decorator(auth)
+    # def dispatch(self, request, *args, **kwargs):
+    #     return super(Order,self).dispatch(request, *args, **kwargs)
+
+    # @method_decorator(auth)  #也可以不写外面，只是单独的为每个get或post请求加@method_decorator(auth)
+    def get(self,reqeust):
+        v = reqeust.COOKIES.get('usernamecook')
+        return render(reqeust,'app02/home.html',{'current_user': v})
+
+    def post(self,reqeust):
+        v = reqeust.COOKIES.get('usernamecook')
+        return render(reqeust,'app02/home.html',{'current_user': v})
+
+
+
+def order(reqeust):
+    # 获取当前已经登录的用户
+    v = reqeust.COOKIES.get('usernamecook')
+    return render(reqeust,'app02/home.html',{'current_user': v})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
